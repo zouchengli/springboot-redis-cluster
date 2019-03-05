9019e6801c2af320b99c4d358cfbee788b3c4d3e
chengli.zou <chengli.zou@gmail.com>
2019-03-05 12:25:39 +0800

    Initial commit

---
+++ src/main/java/site/clzblog/springboot/redis/cluster/SpringbootRedisClusterApplication.java
@@ -0,0 +1,37 @@
+package site.clzblog.springboot.redis.cluster;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootRedisClusterApplication {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public SpringbootRedisClusterApplication(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @GetMapping("set/value")
+    public String setValue(@RequestParam("key") String key, @RequestParam("value") String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+        return String.format("Key:%s,Value:%s saved", key, value);
+    }
+
+    @GetMapping("get/value")
+    public String getValue(@RequestParam("key") String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisClusterApplication.class, args);
+    }
+
+}
